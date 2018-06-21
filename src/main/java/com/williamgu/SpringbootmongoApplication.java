8c518574bc3803388b2ea2d345083b410a7e6d1c
William Gu <ywgu@yahoo.com>
2018-06-21 15:38:59 +0800

    init trial

---
+++ src/main/java/com/williamgu/SpringbootmongoApplication.java
@@ -0,0 +1,12 @@
+package com.williamgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmongoApplication.class, args);
+	}
+}
